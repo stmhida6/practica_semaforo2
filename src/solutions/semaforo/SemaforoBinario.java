23be84db3cef8552731c6859e165689fcb772eb3
mynor hidalgo <stmhida6@upana.edu.gt>
2024-08-23 20:19:23 -0600

    usando Semaphore class

--- src/solutions/semaforo/SemaforoBinario.java
+++ src/solutions/semaforo/SemaforoBinario.java
@@ -1,19 +1,35 @@
 package solutions.semaforo;
+import java.util.concurrent.Semaphore;
 
 public class SemaforoBinario {
 
-    private  boolean control =true;
+ /*   private boolean control = true;
 
     public synchronized void acquire() throws InterruptedException {
         while (!control) {
 
-                wait();
+            wait();
 
         }
         control = false;
     }
-    public synchronized void release () {
+
+    public synchronized void release() {
         control = true;
         notify();
+    }*/
+
+    private final Semaphore semaphore;
+
+    public SemaforoBinario() {
+        this.semaphore = new Semaphore(1);
+    }
+
+    public void acquire() throws InterruptedException {
+        semaphore.acquire();
+    }
+
+    public void release() {
+        semaphore.release();
     }
 }
